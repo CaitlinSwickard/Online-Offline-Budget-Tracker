9b6f1e6556d2bddd85dfdf6ea8df14ebfc05d73f
Caitlin Swickard <caitlin6029@gmail.com>
2021-09-06 18:27:56 -0600

    created service worker cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,75 @@
-console.log("Hello from service worker!")
+// all the files/images to cache when offline
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/indexedDB.js",
+  "/styles.css",
+  "/manifest.webmanifest",
+  "/icons/icon-512x512.png",
+  "/icons/icon-192x192.png",
+];
+
+//cache all the static files
+const CACHE_NAME = "static-cache-v2";
+//cache all the data
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install phase
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Files pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  // wait phase/skip waiting
+  self.skipWaiting();
+});
+
+// activate phase
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key === CACHE_NAME && key === DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// fetch api data
+self.addEventListener("fetch", function (evt) {
+  // cache successful requests to the API 
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+    return;
+  }
+  // if the request is not for the API, serve static assets using "offline-first" approach.
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
