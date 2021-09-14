5046b6c5d9ad39f98d6ad60cdb3c850671bd852c
Caitlin Swickard <caitlin6029@gmail.com>
2021-09-13 21:50:05 -0600

    fixing server connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -23,7 +23,9 @@ mongoose.connect(
     useCreateIndex: true,
     useFindAndModify: false
   }
-);
+)
+  .then(() => console.log('Connected to DB'))
+  .catch(e => console.log(e));
 
 
 // routes
